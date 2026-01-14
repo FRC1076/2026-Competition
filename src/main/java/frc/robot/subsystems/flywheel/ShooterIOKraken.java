cb1c946b4e56421e2603c617ed81e6ba3a5cb8f5
pikitty314 <pikitty314@users.noreply.github.com>
2026-01-14 16:19:44 -0500

    Finish up importing library

---
+++ src/main/java/frc/robot/subsystems/flywheel/ShooterIOKraken.java
@@ -0,0 +1,9 @@
+package frc.robot.subsystems.flywheel;
+
+import com.ctre.phoenix6.configs.TalonFXConfigurator;
+import com.ctre.phoenix6.hardware.TalonFX;
+
+public class ShooterIOKraken {
+    // Create a motor, configuration, and unit converter here
+    
+}
