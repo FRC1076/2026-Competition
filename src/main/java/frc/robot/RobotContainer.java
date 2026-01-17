2557cc99b0b2fa34a7e7fef909996632c2fff112
1076software <93355985+1076software@users.noreply.github.com>
2026-01-17 17:11:28 -0500

    This is why we have branches

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -7,6 +7,7 @@ package frc.robot;
 import frc.robot.Constants.OIConstants;
 import frc.robot.Constants.SystemConstants;
 import frc.robot.Constants.SystemConstants.RobotMode;
+import frc.robot.FieldConstants.AprilTagLayoutType;
 import frc.robot.subsystems.flywheel.FlywheelIODisabled;
 import frc.robot.subsystems.flywheel.FlywheelIOKraken;
 import frc.robot.subsystems.flywheel.FlywheelSubsystem;
@@ -18,8 +19,11 @@ import frc.robot.subsystems.turret.TurretConstants;
 import frc.robot.subsystems.turret.TurretIODisabled;
 import frc.robot.subsystems.turret.TurretIOKraken;
 import frc.robot.subsystems.turret.TurretSubsystem;
+import frc.robot.subsystems.vision.Vision;
+import frc.robot.subsystems.vision.VisionIONorthstar;
 import lib.hardware.hid.SamuraiPS5Controller;
 import lib.hardware.hid.SamuraiXboxController;
+import edu.wpi.first.apriltag.AprilTagFieldLayout;
 import edu.wpi.first.wpilibj.Threads;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.Command;
@@ -38,6 +42,7 @@ public class RobotContainer {
     private final TurretSubsystem m_turret;
     private final FlywheelSubsystem m_flywheel;
     private final HoodSubsystem m_hood;
+    private final Vision m_vision;
 
     // Controllers
     private final SamuraiPS5Controller m_driverController =
@@ -57,6 +62,10 @@ public class RobotContainer {
             m_flywheel = new FlywheelSubsystem(new FlywheelIODisabled());
             m_hood = new HoodSubsystem(new HoodIODisabled());
         }
+
+        AprilTagLayoutType type = FieldConstants.defaultAprilTagType;
+        m_vision = new Vision(type, 
+            new VisionIONorthstar(type, 0));
 
         // Configure the trigger bindings
         configureBindings();
