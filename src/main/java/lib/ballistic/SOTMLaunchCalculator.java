269e74b9337e127b5e5acab669a82ff9f7e08ac8
1076software <1076software@gmail.com>
2026-03-11 19:50:56 -0400

    Continue separation

--- src/main/java/lib/ballistic/SOTMLaunchCalculator.java
+++ src/main/java/lib/ballistic/SOTMLaunchCalculator.java
@@ -5,17 +5,10 @@ import org.littletonrobotics.junction.Logger;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
-import edu.wpi.first.math.interpolation.InterpolatingDoubleTreeMap;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import lib.ballistic.CommonLookupTable.ShotTable;
 
 public class SOTMLaunchCalculator {
-    /** Meters away from hub to hood angle in radians */
-    private static final InterpolatingDoubleTreeMap distanceToHoodAngleMap = CommonLookupTable.hubTable.hoodAngle();
-    /** Meters away from hub to flywheel speed in radians per second */
-    private static final InterpolatingDoubleTreeMap distanceToFlywheelSpeedMap = CommonLookupTable.hubTable.flywheelSpeed();
-    /** Meters away from hub to time of flight in seconds */
-    private static final InterpolatingDoubleTreeMap distanceToTimeOfFlightMap = CommonLookupTable.hubTable.timeOfFlight();
-
     /** Latency for loop time */
     private static final double latencySecs = 0.02;
     /** Number of iterations recurse through the time of flight calculation */
@@ -24,7 +17,8 @@ public class SOTMLaunchCalculator {
     public static CommonShotSolution calculate(
         Pose2d turretPose,
         Pose2d targetPose,
-        ChassisSpeeds turretVelocity
+        ChassisSpeeds turretVelocity,
+        ShotTable shotTable
     ) {
         Pose2d virtualTarget = targetPose;
         Translation2d turretToTarget = new Translation2d();
@@ -35,7 +29,7 @@ public class SOTMLaunchCalculator {
         for (int i = 0; i < iterations; i++) {
             turretToTarget = virtualTarget.relativeTo(turretPose).getTranslation();
             turretToTargetDistanceMeters = turretToTarget.getNorm();
-            timeOfFlightSecs = distanceToTimeOfFlightMap.get(turretToTargetDistanceMeters);
+            timeOfFlightSecs = shotTable.timeOfFlight().get(turretToTargetDistanceMeters);
 
             // Shift the actual target by the velocity times time of flight
             virtualTarget = targetPose.relativeTo(
@@ -50,8 +44,8 @@ public class SOTMLaunchCalculator {
         }
 
         double turretAngleRadians = turretToTarget.getAngle().getRadians();
-        double hoodAngleRadians = distanceToHoodAngleMap.get(turretToTargetDistanceMeters);
-        double flywheelSpeedRadPerSec = distanceToFlywheelSpeedMap.get(turretToTargetDistanceMeters);
+        double hoodAngleRadians = shotTable.hoodAngle().get(turretToTargetDistanceMeters);
+        double flywheelSpeedRadPerSec = shotTable.flywheelSpeed().get(turretToTargetDistanceMeters);
 
         Logger.recordOutput("ShotCalculator/TurretPose", turretPose);
         Logger.recordOutput("ShotCalculator/TurretVelocity", turretVelocity);
