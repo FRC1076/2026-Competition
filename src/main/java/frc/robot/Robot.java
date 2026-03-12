06597a713176bddadf55a36b60c71df370e5638e
1076software <1076software@gmail.com>
2026-03-11 21:08:12 -0400

    fix pdh

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -78,6 +78,10 @@ public class Robot extends LoggedRobot {
         AutoLogOutputManager.addPackage("lib");
 
         // Register PDH?
+        // Enable the switchable channel
+        final PowerDistribution pdh = new PowerDistribution(CANConstants.kPdhCanId, ModuleType.kRev);
+        pdh.setSwitchableChannel(SystemConstants.kEnableSwitchablePDHChannel);
+        pdh.close();
         LoggedPowerDistribution.getInstance(CANConstants.kPdhCanId, ModuleType.kRev);
 
         Logger.start(); // Start logging! No more data receivers, replay sources, or metadata values may be added.
@@ -91,11 +95,6 @@ public class Robot extends LoggedRobot {
         // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
         // autonomous chooser on the dashboard.
         m_robotContainer = new RobotContainer();
-
-        // Enable the switchable channel
-        final PowerDistribution pdh = new PowerDistribution();
-        pdh.setSwitchableChannel(SystemConstants.kEnableSwitchablePDHChannel);
-        pdh.close();
 
         if (SystemConstants.kEnableRTPriority) {
             CommandScheduler.getInstance().schedule(RobotContainer.threadCommand());
